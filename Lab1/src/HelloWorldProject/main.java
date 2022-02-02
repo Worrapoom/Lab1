5ea88746d869b57ff347e6ae74d3713deb144510
WORRAPOOM\Worrapoom <Worrapoom219@gmail.com>
2022-02-02 15:33:49 +0700

    Lab1

---
+++ Lab1/src/HelloWorldProject/main.java
@@ -0,0 +1,14 @@
+package HelloWorldProject;
+
+public class main {
+		
+	public main() {
+		//TODO Auto-generated constructor stub
+	}//end function
+	
+	public static void main(String[] args) {
+		System.out.println( "Hello World" );
+		//TODO Auto-generated method stub
+	}//end function
+	
+}//end class
